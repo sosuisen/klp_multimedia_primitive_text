08545d6d843111fe7e9ac5a08640329f4e706f36
sosuisen <hidekazu.kubota@gmail.com>
2025-03-27 07:13:18 +0900

    fix: v8 for primitives

--- primitives.js
+++ primitives.js
@@ -1,7 +1,8 @@
 import * as PIXI from 'pixi.js';
 
-const app = new PIXI.Application({ antialias: true, backgroundColor: 0xa0a0ff });
-document.body.appendChild(app.view);
+const app = new PIXI.Application();
+await app.init({ antialias: true, backgroundColor: 0xa0a0ff });
+document.body.appendChild(app.canvas);
 
 const graphics = new PIXI.Graphics();
 
@@ -10,82 +11,74 @@ const graphics = new PIXI.Graphics();
 // https://pixijs.download/v7.x/docs/PIXI.Graphics.html#lineTo
 // LineStyle
 // https://pixijs.download/v7.x/docs/PIXI.Graphics.html#lineStyle
-graphics.lineStyle(4, 0xffd900, 0.5);
-graphics.moveTo(30, 50);
-graphics.lineTo(100, 100);
-graphics.lineTo(200, 50);
-graphics.lineTo(300, 100);
+
+graphics.moveTo(30, 50)
+    .lineTo(100, 100)
+    .lineTo(200, 50)
+    .lineTo(300, 100)
+    .stroke({ width: 4, color: 0xffd900, alpha: 0.5 });
 
 // Shape（閉じた図形）
 // https://pixijs.download/v7.x/docs/PIXI.Graphics.html#beginFill
 // https://pixijs.download/v7.x/docs/PIXI.Graphics.html#closePath
-graphics.beginFill(0xff0000, 0.3);
-graphics.lineStyle(4, 0xffd900, 1);
-graphics.moveTo(50, 150);
-graphics.lineTo(250, 150);
-graphics.lineTo(100, 200);
-graphics.lineTo(50, 150);
-graphics.closePath();
-graphics.endFill();
+graphics.moveTo(50, 150)
+    .lineTo(250, 150)
+    .lineTo(100, 200)
+    .closePath()
+    .fill({ color: 0xff0000, alpha: 0.3 })
+    .stroke({ width: 4, color: 0xffd900 });
 
 // Polygon（多角形）
 // https://pixijs.download/v7.x/docs/PIXI.Graphics.html#drawPolygon
 const path = [100, 240, 200, 360, 280, 320, 230, 470, 50, 400];
-graphics.lineStyle(0);
 // 指定のテクスチャで塗りつぶす
-graphics.beginTextureFill({ texture: PIXI.Texture.from('star.png') });
-graphics.drawPolygon(path);
-graphics.endFill();
+await PIXI.Assets.load('star.png');
+graphics.poly(path)
+    .fill({ texture: PIXI.Texture.from('star.png'), textureSpace: 'global' });
 
 // Rectangle（矩形）
-graphics.lineStyle(0);
-graphics.beginFill(0xde3249);
-graphics.drawRect(350, 30, 100, 100);
-graphics.endFill();
+graphics.rect(350, 30, 100, 100)
+    .fill(0xde3249);
 
 // Rectangle（矩形＋枠線）
-graphics.lineStyle(10, 0xffbd01, 1);
-graphics.beginFill(0xc34288);
-graphics.drawRect(350, 150, 100, 100);
-graphics.endFill();
+graphics.rect(350, 150, 100, 100)
+    .fill(0xc34288)
+    .stroke({ width: 10, color: 0xffbd01 });
 
 // Rounded Rectangle（角丸の矩形）
-graphics.lineStyle(2, 0xff00ff, 1);
-graphics.beginFill(0x650a5a, 0.65);
-graphics.drawRoundedRect(350, 280, 100, 100, 16);
-graphics.endFill();
+graphics.roundRect(350, 280, 100, 100, 16)
+    .fill({ color: 0x650a5a, alpha: 0.65 })
+    .stroke({ width: 2, color: 0xff00ff });
 
 // Circle（円）
 // https://pixijs.download/v7.x/docs/PIXI.Graphics.html#drawCircle
-graphics.lineStyle(10, 0xffbd01, 1);
-graphics.beginFill(0xc34288, 1);
-graphics.drawCircle(600, 80, 50);
-graphics.endFill();
+graphics.circle(600, 80, 50)
+    .fill(0xc34288)
+    .stroke({ width: 10, color: 0xffbd01 });
 
 // Ellipse（楕円）
 // https://pixijs.download/v7.x/docs/PIXI.Graphics.html#drawEllipse
-graphics.lineStyle(2, 0xffffff, 1);
-graphics.beginFill(0xaa4f08, 1);
-graphics.drawEllipse(600, 200, 80, 50);
-graphics.endFill();
-
+graphics.ellipse(600, 200, 80, 50)
+    .fill(0xaa4f08)
+    .stroke({ width: 2, color: 0xffffff });
 
 // Arc（円弧）
 // https://pixijs.download/v7.x/docs/PIXI.Graphics.html#arc
-graphics.lineStyle(4, 0xff00ff, 1);
-graphics.moveTo(520, 320);
-graphics.arc (520, 320, 50, 0, Math.PI/2, false);
-graphics.closePath();
+graphics.moveTo(520, 320)
+    .arc(520, 320, 50, 0, Math.PI / 2, false)
+    .closePath()
+    .stroke({ width: 4, color: 0xff00ff });
 
-graphics.moveTo(650, 320);
-graphics.arc (650, 320, 50, 0, Math.PI/2, true);
-graphics.closePath();
+graphics.moveTo(650, 320)
+    .arc(650, 320, 50, 0, Math.PI / 2, true)
+    .closePath()
+    .stroke({ width: 4, color: 0xff00ff });
 
 // Quadratic Bezier Curve（2次ベジェ曲線）
-https://pixijs.download/v7.x/docs/PIXI.Graphics.html#quadraticCurveTo
-graphics.lineStyle(3, 0x00ffff, 1);
-graphics.moveTo(500, 450);
-graphics.quadraticCurveTo(550, 400, 600, 450);
-graphics.quadraticCurveTo(650, 500, 700, 450);
+// https://pixijs.download/v7.x/docs/PIXI.Graphics.html#quadraticCurveTo
+graphics.moveTo(500, 450)
+    .quadraticCurveTo(550, 400, 600, 450)
+    .quadraticCurveTo(650, 500, 700, 450)
+    .stroke({ width: 3, color: 0x00ffff });
 
 app.stage.addChild(graphics);
