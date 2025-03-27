0c947e26f4dee6d1ddc92079bf46c31ae5fdae3f
sosuisen <hidekazu.kubota@gmail.com>
2025-03-27 13:16:33 +0900

    fix: v8

--- hatten.js
+++ hatten.js
@@ -9,33 +9,34 @@ PixiPlugin.registerPIXI(PIXI);
 let screenWidth = window.innerWidth;
 let screenHeight = window.innerHeight;
 
-const app = new PIXI.Application({ backgroundColor: "#ffffff", width: screenWidth, height: screenHeight });
+const app = new PIXI.Application();
+await app.init({ backgroundColor: "#ffffff", width: screenWidth, height: screenHeight });
 
 app.ticker.stop();
 gsap.ticker.add(time => {
     app.ticker.update();
 });
-document.body.appendChild(app.view);
+document.body.appendChild(app.canvas);
 
-const style = new PIXI.TextStyle({
-    fontSize: 128,
-    fill: '#0b2f80',
-    lineJoin: 'round',
+const text = new PIXI.Text({
+    text: 'KCG',
+    style: {
+        fontSize: 128,
+        fill: '#0b2f80',
+        lineJoin: 'round',
+    },
+    anchor: 0.5,
+    y: screenHeight / 2,
 });
-
-const text = new PIXI.Text('KCG', style);
-
 app.stage.addChild(text);
-text.anchor.set(0.5);
 text.x = -text.width / 2;
-text.y = screenHeight / 2;
 
 let tl = gsap.timeline();
 
 const setTimeline = () => {
-    tl.to(text, { pixi: { x: screenWidth / 2 }, duration: 1.5, ease: 'power3.out' });
-    tl.to(text, { pixi: { scaleX: 2.0 }, duration: 0.5, yoyo: true, repeat: 1 });
-    tl.to(text, { pixi: { x: screenWidth + text.width / 2 }, duration: 1.5, delay: 3, ease: 'power3.out' });
+    tl.to(text, { pixi: { x: screenWidth / 2 }, duration: 1.5, ease: 'power3.out' })
+        .to(text, { pixi: { scaleX: 2.0 }, duration: 0.5, yoyo: true, repeat: 1 })
+        .to(text, { pixi: { x: screenWidth + text.width / 2 }, duration: 1.5, delay: 3, ease: 'power3.out' });
 }
 setTimeline();
 tl.pause();
